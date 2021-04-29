b4f937b3de6647425fd8f3315fb4674ddc0dfa6f
Ubuntu <ubuntu@ip-172-31-22-83.us-east-2.compute.internal>
2021-04-29 15:06:04 +0000

    bleh

---
+++ R/navar_lstm.R
@@ -0,0 +1,15 @@
+#' navar_lstm
+#'
+#' @param num_nodes The number of time series (N)
+#' @param num_hidden Number of hidden units per layer
+#' @param hidden_layers Number of hidden layers
+#' @param dropout Dropout probability of units in hidden layers
+#' @param bias_scale Scaling factor for bias
+#'
+#' @return
+#' @export
+#'
+#' @examples
+navar_lstm <- function(num_nodes, num_hidden, hidden_layers, dropout, bias_scale=0.0001) {
+  
+}
